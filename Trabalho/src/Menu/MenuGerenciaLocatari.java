c8963bf298f53e5b65b3e1fca8f597185169e01d
PedroHhenriq <100311889+PedroHhenriq@users.noreply.github.com>
2022-03-17 20:40:20 -0300

    Add files via upload

--- Trabalho/src/Menu/MenuGerenciaLocatari.java
+++ Trabalho/src/Menu/MenuGerenciaLocatari.java
@@ -1,107 +1,105 @@
+package Menu;
 
+import java.util.Scanner;
 
-	package Menu;
-
-	import java.util.Scanner;
-
-	public class MenuGerenciaLocatari {
+public class MenuGerenciaLocatari {
+	
+	public static void MenuLocatario() {
 		
-		public static void MenuLocatario() {
+		System.out.println("------------- Menu [GerÍncia de Locat·rios] ---------------\n");
+		System.out.println("                 1 - Cadastrar Locat·rio                     ");
+		System.out.println("                 2 - Buscar Locat·rio                        ");
+		System.out.println("                 3 - Pesquisar Locat·rio                     ");
+		System.out.println("                 4 - Excluir Locat·rio                       ");
+		System.out.println("                                                             ");
+		System.out.println("                 Escolha uma dessas opÁıes:                  ");
 			
-			System.out.println("------------- Menu [Ger√™ncia de Locat√°rios] ---------------\n");
-			System.out.println("                 1 - Cadastrar Locat√°rio                     ");
-			System.out.println("                 2 - Buscar Locat√°rio                        ");
-			System.out.println("                 3 - Pesquisar Locat√°rio                     ");
-			System.out.println("                 4 - Excluir Locat√°rio                       ");
-			System.out.println("                                                             ");
-			System.out.println("                 Escolha uma dessas op√ß√µes:                  ");
-				
-			}
-		
-		public static void cadastrarlocatario() {
-			String escolhaTipo;
-			char escolha = escolhaTipo.charAt(0);
-			Scanner cadastrar=new Scanner(System.in);	
-			System.out.println("Cadastrando Locat√°rio...");
-			System.out.println("Digite f se quer alugar como uma pessoa fisica , ou  j como  pessoa juridica...");
-			 escolhaTipo = cadastrar.nextLine();
-			if((escolha == 'f')|| (escolha == 'F')) {
-			System.out.println("Coloque o seu nome completo:");
-			String nomeCompleto=cadastrar.nextLine();
-			System.out.println("Coloque o seu Cpf:");
-			String cpf=cadastrar.nextLine();
-			System.out.println("Coloque o seu estado civil:");
-			String estadoCivil=cadastrar.nextLine();
-			
-			System.out.println("Dados cadastrados: ");
-			System.out.println("nome completo: "+nomeCompleto);
-			System.out.println("cpf: "+cpf);
-			System.out.println("estado civil: "+estadoCivil);
-			
-				
-			}else if((escolha == 'j')|| (escolha == 'J')) {
-				
-			System.out.println("Coloque o nome social da sua empresa:");
-			String nomeEmpresa=cadastrar.nextLine();
-			System.out.println("Coloque o nome da pessoa autorizada a realizar loca√ß√µes:");
-			String nome=cadastrar.nextLine();
-			System.out.println("Dados cadastrados: ");
-			System.out.println("nome da Empresa: "+nomeEmpresa);
-			System.out.println("Email: "+nome);
-			
-				
-			}
-			System.out.println("Coloque o seu endere√ßo:");
-			String Endereco=cadastrar.nextLine();
-			System.out.println("Coloque o seu Email:");
-			String Email=cadastrar.nextLine();
-			System.out.println("Coloque o seu Telefone:");
-			String Telefone=cadastrar.nextLine();
-			System.out.println("Endere√ßo: "+Endereco);
-			System.out.println("Email: "+Email);
-			System.out.println("Telefone: "+Telefone);
-		   
 		}
-		public static void buscarlocatario() {
-			System.out.println("Metodo Buscar Loc√°tario...");
-		}
+	
+	public static void cadastrarlocatario() {
+		Scanner cadastrar=new Scanner(System.in);
+		System.out.println("Cadastrando Locat·rio...");
+		System.out.println("Digite f se quer alugar como uma pessoa fisica , ou  j como  pessoa juridica...");
+		String escolhaTipo;
+		escolhaTipo = cadastrar.nextLine();
+		char escolha = escolhaTipo.charAt(0);
+		if((escolha == 'f')|| (escolha == 'F')) {
+		System.out.println("Coloque o seu nome completo:");
+		String nomeCompleto=cadastrar.nextLine();
+		System.out.println("Coloque o seu Cpf:");
+		String cpf=cadastrar.nextLine();
+		System.out.println("Coloque o seu estado civil:");
+		String estadoCivil=cadastrar.nextLine();
 		
-		public static void pesquisarlocatario() {
-			System.out.println("Metodo Pesquisar Loc√°tario...");
-		}
+		System.out.println("Dados cadastrados: ");
+		System.out.println("nome completo: "+nomeCompleto);
+		System.out.println("cpf: "+cpf);
+		System.out.println("estado civil: "+estadoCivil);
 		
-		public static void excluirlocatario() {
-			System.out.println("Metodo Excluir Loc√°tario...");
-		}
+			
+		}else if((escolha == 'j')|| (escolha == 'J')) {
+			
+		System.out.println("Coloque o nome social da sua empresa:");
+		String nomeEmpresa=cadastrar.nextLine();
+		System.out.println("Coloque o nome da pessoa autorizada a realizar locaÁıes:");
+		String nome=cadastrar.nextLine();
+		System.out.println("Dados cadastrados: ");
+		System.out.println("Nome da Empresa: "+nomeEmpresa);
+		System.out.println("Nome da pessoa: "+nome);
 		
-		public static void main(String [] args) {
 			
-			int escolha=0;
+		}
+		System.out.println("Coloque o seu endereÁo:");
+		String Endereco=cadastrar.nextLine();
+		System.out.println("Coloque o seu Email:");
+		String Email=cadastrar.nextLine();
+		System.out.println("Coloque o seu Telefone:");
+		String Telefone=cadastrar.nextLine();
+		System.out.println("Dados cadastrados: ");
+		System.out.println("EndereÁo: "+Endereco);
+		System.out.println("Email: "+Email);
+		System.out.println("Telefone: "+Telefone);
+	   
+	}
+	public static void buscarlocatario() {
+		System.out.println("Metodo Buscar Loc·tario...");
+	}
+	
+	public static void pesquisarlocatario() {
+		System.out.println("Metodo Pesquisar Loc·tario...");
+	}
+	
+	public static void excluirlocatario() {
+		System.out.println("Metodo Excluir Loc·tario...");
+	}
+	
+	public static void main(String [] args) {
+		
+		int escolha=0;
+		
+		Scanner ler=new Scanner(System.in);
+		
+		MenuLocatario();
+		
+		escolha=ler.nextInt();
+		
+		switch(escolha) {
+		case 1:
+			cadastrarlocatario();
+			break;
+		case 2:
+			buscarlocatario();
+			break;
+		case 3:
+			pesquisarlocatario();
+			break;
+		case 4:
+			excluirlocatario();
+			break;
 			
-			Scanner ler=new Scanner(System.in);
+			default:
+				System.out.println("Escolha inv·lida, tente novamente!");
 			
-			MenuLocatario();
-			
-			escolha=ler.nextInt();
-			
-			switch(escolha) {
-			case 1:
-				cadastrarlocatario();
-				break;
-			case 2:
-				buscarlocatario();
-				break;
-			case 3:
-				pesquisarlocatario();
-				break;
-			case 4:
-				excluirlocatario();
-				break;
-				
-				default:
-					System.out.println("Escolha inv√°lida, tente novamente!");
-				
-			}
 		}
 	}
-
+}
