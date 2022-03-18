9c2411827239a61fddf9b6d29c2e132362649958
Matheus Henrique <matheushenriquep285@gmail.com>
2022-03-18 19:42:53 -0300

    Correções em CadastrarVeículos.java
    Correção de algumas indentação, deixando o código mais legível e corrigindo testes de comparação.

--- Trabalho/src/Menu/CadastrarVeículos.java
+++ Trabalho/src/Menu/CadastrarVeículos.java
@@ -5,139 +5,169 @@ import java.util.Scanner;
 public class CadastrarVeículos {
 	
 	public static void cadastro() {
-		Scanner cadastrarV=new Scanner(System.in);		
+
+		Scanner cadastrarV = new Scanner(System.in);
+
+		// Obtendo os dados iniciais do veículo.
 		System.out.println("Cadastrando...");
 		System.out.println("Coloque a Marca do Veículo:");
-		String Marca=cadastrarV.nextLine();
+		String Marca = cadastrarV.nextLine();
+
 		System.out.println("Coloque o Modelo do Veículo:");
-		String Modelo=cadastrarV.nextLine();
+		String Modelo = cadastrarV.nextLine();
+
 		System.out.println("Coloque o Ano de Fabricação:");
-		String AnoFab=cadastrarV.nextLine();
+		String AnoFab = cadastrarV.nextLine();
+
 		System.out.println("Coloque o Ano de Modelo:");
-		String AnoMod=cadastrarV.nextLine();
+		String AnoMod = cadastrarV.nextLine();
+
 		System.out.println("Coloque o Número do Renavam:");
-		String Renavam=cadastrarV.nextLine();
-		System.out.println("Coloque a Capacidade do TANQUE:");
-		String CapacidadedoTanque=cadastrarV.nextLine();
+		String Renavam = cadastrarV.nextLine();
+
+		System.out.println("Coloque a Capacidade do Tanque:");
+		String CapacidadeTanque = cadastrarV.nextLine();
+
+		// Mostrando os dados digitados.
+		System.out.println("\n\n\n");
 		System.out.println("Dados cadastrados: ");
-		System.out.println("Marca: "+Marca);
-		System.out.println("Modelo: "+Modelo);
-		System.out.println("Ano de Fabricação: "+AnoFab);
-		System.out.println("Ano Modelo: "+AnoMod);
-		System.out.println("Renavam: "+Renavam);
-		System.out.println("Capacidade do Tanque: "+CapacidadedoTanque);
+		System.out.println("Marca: " + Marca);
+		System.out.println("Modelo: " + Modelo);
+		System.out.println("Ano de Fabricação: " + AnoFab);
+		System.out.println("Ano Modelo: " + AnoMod);
+		System.out.println("Renavam: " + Renavam);
+		System.out.println("Capacidade do Tanque: " + CapacidadeTanque);
 	}
+
 	public static void CadastrarVeiculo() {
-		
+
 	System.out.println("-------------------- Cadastro de Veículos -----------------\n");
 	System.out.println("                 1 - Cadastrar Veículo de Passeio            ");
 	System.out.println("                 2 - Cadastrar Veículo Utilitário            ");
 	System.out.println("                 3 - Cadastrar Motocicleta                   ");
 	System.out.println("                                                             ");
 	System.out.println("                 Escolha uma dessas opções:                  ");
+
 	}
 	
 	public static void CadastrarPasseio() {
-		Scanner cadastrarP=new Scanner(System.in);
+
+		Scanner cadastrarP = new Scanner(System.in);
+
+		// Obtendo os dados dos itens opcionais de veículos de passeio.
 		System.out.println("Cadastrando Veículo de Passeio...");
 		System.out.println("Coloque a Categoria do Veículo:");
-		String CategoriaVeiculo=cadastrarP.nextLine();
-		System.out.println("O veículo possui Ar Condicionado? Digite Sim/sim se possuir e Não/não se não possuir:");
-		String Arcondicionado=cadastrarP.nextLine();
-		String Ar;
-		Ar=Arcondicionado;
-		if(Ar=="Sim"||Ar=="sim") {
-			System.out.print("Ar Condicionado: Possui");
-		} else if(Ar=="Não"||Ar=="não") {
-			System.out.print("Ar Condicionado: Não possui");
+		String CategoriaVeiculo = cadastrarP.nextLine();
+
+		System.out.println("O veículo possui Ar-Condicionado? (Digite sim ou não)");
+		String Ar = cadastrarP.nextLine();
+		Boolean ArCondicionado = true; 
+		// Usando o método “.equalsIgnoreCase()” para verificar o que foi digitado. Essa estrutura é repetida ao longo do código.
+		if (Ar.equalsIgnoreCase("sim")) {
+			System.out.print("Ar-Condicionado: Possui");
+		} else if (Ar.equalsIgnoreCase("não")) {
+			ArCondicionado = false;
+			System.out.print("Ar-Condicionado: Não possui");
 		}
-		System.out.println("O veículo possui Direção Hidraulica? Digite Sim/sim se possuir e Não/não se não possuir:");
-		String DirecaoHidraulica=cadastrarP.nextLine();;
-		String Direcao;
-		Direcao=DirecaoHidraulica;
-		if(Direcao=="Sim"||Direcao=="s") {
+
+		System.out.println("O veículo possui Direção Hidraulica? (Digite sim ou não)");
+		String Direcao = cadastrarP.nextLine();
+		Boolean DirecaoHidraulica = true;
+		if (Direcao.equalsIgnoreCase("sim")) {
 			System.out.print("Direcao Hidraulica: Possui");
-		} else if(Direcao=="Não"||Direcao=="não") {
+		} else if (Direcao.equalsIgnoreCase("não")) {
+			DirecaoHidraulica = false;
 			System.out.print("Direcao Hidraulica: Não possui");
 		}
-		System.out.println("O veículo tem Câmbio Automático? Digite Sim/sim se possuir e Não/não se não possuir:");
-		String CambioAutomatico=cadastrarP.nextLine();;
-		String Cambio;
-		Cambio=CambioAutomatico;
-		if((Cambio=="Sim")||(Cambio=="sim")) {
+
+		System.out.println("O veículo tem Câmbio Automático? (Digite sim ou não)");
+		String Cambio = cadastrarP.nextLine();
+		Boolean CambioAutomatico = true;
+		if (Cambio.equalsIgnoreCase("sim")) {
 			System.out.print("Câmbio Automático: Possui");
-		} else if(Cambio=="Não"||Cambio=="não") {
+		} else if (Cambio.equalsIgnoreCase("não")) {
+			CambioAutomatico = false;
 			System.out.print("Câmbio Automático: Não possui");
 		}
+
+		System.out.println("\n\n\n");
 		System.out.println("Dados cadastrados: ");
-		System.out.println("Categoria do Veículo: "+CategoriaVeiculo);
-		System.out.println("Arcondicionado: "+Ar);
-		System.out.println("Direção Hidráulica: "+Direcao);
-		System.out.println("Câmbio Automa´tico: "+Cambio);
+		System.out.println("Categoria do Veículo: "+ CategoriaVeiculo);
+		System.out.println("Ar-Condicionado: " + Ar);
+		System.out.println("Direção Hidráulica: " + Direcao);
+		System.out.println("Câmbio Automático: " + Cambio);
 	}
 	
 	public static void CadastrarUtilitario() {
-		Scanner cadastrarU=new Scanner(System.in);	
+
+		// Obtendo os dados dos itens opcionais de veículos utilitários.
 	    System.out.println("Cadastrando Utilitario");
-	    System.out.println("O veículo é do tipo Carga o Passageiro? Digite carga ou passageiro");		
-		String EscolhaU=cadastrarU.nextLine();
-		String EscolhaUt;
-	    EscolhaUt=EscolhaU;
-		if((EscolhaU=="Carga")||(EscolhaU=="carga")) {
-			System.out.print("Veículo de carga");
-			
+	    System.out.println("O veículo é do tipo Carga o Passageiro? (Digite carga ou passageiro)");		
+		Scanner cadastrarU = new Scanner(System.in);
+		String EscolhaU = cadastrarU.nextLine();
+		if (EscolhaU.equalsIgnoreCase("carga")) {
+			System.out.println("Veículo de Carga");
 		}  
-			else if((EscolhaU=="Passageiro")||(EscolhaU=="passageiro")) {
-				System.out.print("Veículo de Passageiro");
-			}
+		else if (EscolhaU.equalsIgnoreCase("passageiro")) {
+			System.out.println("Veículo de Passageiro");
+		}
 	}
+
 	public static void CadastrarMotocicleta() {
-		Scanner cadastrarM=new Scanner(System.in);	
+
+		Scanner cadastrarM = new Scanner(System.in);
 		System.out.println("Cadastrando Motocicleta...");
-		System.out.println("O veículo tem Controle de Tração? Digite Sim/sim se possuir e Não/não se não possuir:");
-		String ControleTracao=cadastrarM.nextLine();;
-		String Controle;
-		Controle=ControleTracao;
-		if((Controle=="Sim")||(Controle=="sim")) {
+
+		System.out.println("O veículo tem Controle de Tração? (Digite sim ou não)");
+		String Controle = cadastrarM.nextLine();;
+		Boolean ControleTracao = true;
+		if (Controle.equalsIgnoreCase("sim")) {
 			System.out.print("Controle Tração: Possui");
-		} else if(Controle=="Não"||Controle=="não") {
+		} else if (Controle.equalsIgnoreCase("não")) {
+			ControleTracao = false;
 			System.out.print("Controle Tração: Não possui");
 		}
-		System.out.println("O veículo tem Freio ABS? Digite Sim/sim se possuir e Não/não se não possuir:");
-		String FreioAbs=cadastrarM.nextLine();
-		String Freio;
-		Freio=FreioAbs;
-		if((Freio=="Sim")||(Freio=="sim")) {
+
+		System.out.println("O veículo tem Freio ABS? (Digite sim ou não)");
+		String Freio = cadastrarM.nextLine();
+		Boolean FreioAbs = true;
+		if (Freio.equalsIgnoreCase("sim")) {
 			System.out.print("Freio Abs: Possui");
-		} else if((Freio=="Não")||(Freio=="não")) {
+		} else if (Freio.equalsIgnoreCase("não")) {
+			FreioAbs = false;
 			System.out.print("Freio Abs: Não possui");
 		}
-		System.out.println("O veículo tem Piloto Automatico? Digite Sim/sim se possuir e Não/não se não possuir:");
-		String PilotoAutomatico=cadastrarM.nextLine();
-		String Piloto;
-		Piloto=PilotoAutomatico;
-		if((Piloto=="Sim")||(Piloto=="sim")) {
+
+		System.out.println("O veículo tem Piloto Automatico? (Digite sim ou não)");
+		String Piloto = cadastrarM.nextLine();
+		Boolean PilotoAutomatico = true;
+		if (Piloto.equalsIgnoreCase("sim")) {
 			System.out.print("Piloto Automatico: Possui");
-		} else if((Piloto=="Não")||(Piloto=="não")) {
+		} else if (Piloto.equalsIgnoreCase("não")) {
+			PilotoAutomatico = false;
 			System.out.print("Piloto Automatico: Não possui");
 		}
-		System.out.println("Digite o Modo de Pilotagem: Cidade/Estrada/Sport/Off-Road");
-		String MododePilotagem=cadastrarM.nextLine();
-		String Modo;
-		Modo=MododePilotagem;
-		if((Piloto=="Cidade")) {
+
+		System.out.println("Digite o Modo de Pilotagem: (Digite Cidade, Estrada, Sport ou Off-Road)");
+		String ModoPilotagem = cadastrarM.nextLine();
+		if (ModoPilotagem.equalsIgnoreCase("cidade")) {
 			System.out.print("Modo de Pilotagem: Cidade");
-		} else if ((Piloto=="Estrada")) {
+
+		} else if (ModoPilotagem.equalsIgnoreCase("estrada")) {
 			System.out.print("Piloto Automatico: Estrada");
-		}     else if ((Piloto=="Sport")){
-			   System.out.print("Modo de Piltogem: Sport");
-		}       else if ((Piloto=="Off-Road")) {
-			      System.out.print("Modo de Pilotagem: Off-Road");
+
+		} else if (ModoPilotagem.equalsIgnoreCase("sport")){
+			System.out.print("Modo de Piltogem: Sport");
+
+		} else if (ModoPilotagem.equalsIgnoreCase("off-road")) {
+			System.out.print("Modo de Pilotagem: Off-Road");
 		}
+
+		System.out.println("\n\n\n");
 		System.out.println("Dados cadastrados: ");
-		System.out.println("Controle de Tração: "+Controle);
-		System.out.println("Freio ABS: "+Freio);
-		System.out.println("Piloto Automático: "+Piloto);
-		System.out.println("Modo de Pilotagem: "+Modo);
+		System.out.println("Controle de Tração: " + Controle);
+		System.out.println("Freio ABS: " + Freio);
+		System.out.println("Piloto Automático: " + Piloto);
+		System.out.println("Modo de Pilotagem: " + ModoPilotagem);
 	}
 }
